3ebfa5b7381fd96d8df202ed66c6f10b3d75ebf1
imvm <ianmanor.dev@gmail.com>
2019-05-26 10:24:42 -0300

    Initial Commit

---
+++ Equipa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Equipa
+//
+//  Created by Ian Manor on 26/05/19.
+//  Copyright © 2019 Ian Manor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
